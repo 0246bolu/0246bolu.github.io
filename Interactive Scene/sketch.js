ca6d9f8d4d43513b79042f2bb637e2c883010d25
Boyd4601148 <4601148@spsd.sk.ca>
2024-09-18 12:17:28 -0600

    Made Building Windows and Character

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -1,19 +1,23 @@
 // Interactive Scene
-// Lucas Boyd
+// Lucas Boyd 
 // 9/16/24
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// let numBuildings = 0;
 let currentBack = 0;
 let buildingRan = false;
+let windowColor = 0;
+
+let carX = 0;
+let carY = 50;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(RGB);
   drawBg();
 }
+
 
 function mouseClicked(){
   if(currentBack<3){
@@ -22,8 +26,8 @@ function mouseClicked(){
   else{
     currentBack=0;
   }
-  drawBg();
   buildingRan = false;
+  drawBg();
 }
 
 function drawBg() {
@@ -31,21 +35,25 @@ function drawBg() {
     background(0, 0, 225);
     fill(255, 255, 0);
     circle(0, 0, 200);
+    windowColor = [0,0,225];
   }
   if(currentBack === 1){
     background(0, 0, 0);
-    fill(255, 255, 0);
+    fill(500);
     circle(0, 0, 200);
+    windowColor = [255,255,0];
   }
   if(currentBack === 2){
     background(0, 0, 100);
     fill(255, 255, 0);
     circle(0, 0, 200);
+    windowColor = [0,0,100];
   }
   if(currentBack === 3){
     background(0, 0, 150);
     fill(255, 255, 0);
     circle(0, 0, 200);
+    windowColor = [0,0,150];
   }
 
   if (buildingRan === false){
@@ -69,12 +77,42 @@ function drawCity() {
     fill(160,82,45);
     rect(buildXPos, buildYPos, widthBuild, -heightBuild);
 
+    fill(windowColor);
+    rect(buildXPos+widthBuild/4, buildYPos-heightBuild/5, widthBuild/8, heightBuild/8);
+    rect(buildXPos+widthBuild/1.5, buildYPos-heightBuild/5, widthBuild/8, heightBuild/8);
+
+    rect(buildXPos+widthBuild/4, buildYPos-heightBuild/2, widthBuild/8, heightBuild/8);
+    rect(buildXPos+widthBuild/1.5, buildYPos-heightBuild/2, widthBuild/8, heightBuild/8);
+
+    rect(buildXPos+widthBuild/4, buildYPos-heightBuild/1.25, widthBuild/8, heightBuild/8);
+    rect(buildXPos+widthBuild/1.5, buildYPos-heightBuild/1.25, widthBuild/8, heightBuild/8);
+
+
     numBuildings++;
   }
-
   buildingRan = true;
 }
 
-function drawCharacter() {
-  
+function draw(){
+  randomSeed(1);
+  if(keyIsPressed){
+    if (key === "d"){
+      buildingRan = false;
+      drawBg();
+      carX += 10;
+    }
+    if (key === "a"){
+      buildingRan = false;
+      drawBg();
+      carX -= 10;
+    }
+    if(carX>=windowWidth){
+      carX=0;
+    }
+    if(carX<=0){
+      carX=windowWidth;
+    }
+  }
+  fill(50,150,255)
+  rect(carX, carY, 100, 100)
 }
