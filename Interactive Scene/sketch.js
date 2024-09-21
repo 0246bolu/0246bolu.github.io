9ceea12d9bc8784e2ca6bc80353bce14c505317d
0246bolu <246bolu@gmail.com>
2024-09-21 16:08:19 -0600

    Add files via upload

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -1,6 +1,6 @@
 // Interactive Scene
 // Lucas Boyd 
-// 9/16/24
+// 9/21/24
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -9,8 +9,12 @@ let currentBack = 0;
 let buildingRan = false;
 let windowColor = 0;
 
-let carX = 0;
+let carX = 20;
 let carY = 90;
+
+let planeIteration = 0;
+let planeColor = 0;
+let colorChange = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -37,19 +41,19 @@ function drawBg() {
     circle(0, 0, 200);
     windowColor = [0,0,225];
   }
-  if(currentBack === 1){
+  else if(currentBack === 1){
     background(0, 0, 0);
     fill(500);
     circle(0, 0, 200);
     windowColor = [255,255,0];
   }
-  if(currentBack === 2){
+  else if(currentBack === 2){
     background(0, 0, 100);
     fill(255, 255, 0);
     circle(0, 0, 200);
     windowColor = [0,0,100];
   }
-  if(currentBack === 3){
+  else if(currentBack === 3){
     background(0, 0, 150);
     fill(255, 255, 0);
     circle(0, 0, 200);
@@ -57,9 +61,9 @@ function drawBg() {
   }
 
   if (buildingRan === false){
+    colorChange = false;
     drawCity();
   }
-
 }
 
 function drawCity() {
@@ -104,12 +108,21 @@ function drawCity() {
 
 function draw(){
   if(keyIsPressed){
+    if (key === "c"  && colorChange===false){
+      if(planeIteration<2){
+        planeIteration++;
+      }
+      else if(planeIteration>=2){
+        planeIteration = 0;
+      }
+      colorChange = true;
+    }
     if (key === "d"){
       buildingRan = false;
       drawBg();
       carX += 10;
     }
-    if (key === "a"){
+    else if (key === "a"){
       buildingRan = false;
       drawBg();
       carX -= 10;
@@ -117,15 +130,41 @@ function draw(){
     if(carX>=windowWidth){
       carX=0;
     }
-    if(carX<=0-250){
+    else if(carX<=-75){
       carX=windowWidth;
     }
   }
 
-  fill(255);
-  ellipse(carX+150, carY+25, 200, 50);
-  triangle(carX+70, carY, carX+140, carY, carX, carY-70);
-  triangle(carX+80, carY, carX+150, carY, carX, carY+140);
-  triangle(carX, carY, carX, carY, carX, carY);
-  rect(carX, carY, 150, 50);
+  if(planeIteration === 0){
+    planeColor = [255,255,255];
+  }
+  else if(planeIteration === 1){
+    planeColor = [255,0,0];
+  }
+  else if(planeIteration === 2){
+    planeColor = [0,128,0];
+  }
+
+  fill(planeColor);
+  ellipse(carX+150/2, carY+25/2, 200/2, 50/2);
+  triangle(carX+70/2, carY, carX+140/2, carY, carX, carY-70/2);
+  triangle(carX+80/2, carY, carX+150/2, carY, carX, carY+140/2);
+  triangle(carX-1/2, carY+50/2, carX+50/2, carY+50/2, carX, carY-70/2);
+  strokeWeight(0);
+  rect(carX, carY, 150/2, 50/2);
+  strokeWeight(1);
+  
+  fill(0);
+  rect(carX+175/2, carY+2/2, 25/2);
+  ellipse(carX+205/2, carY+15/2, 50/2, 25/2);
+
+  let windiesNum = 0;
+  let windowOffset = 15;
+
+  while(windiesNum <= 3){
+    fill(0);
+    circle(carX+windowOffset, carY+25/2, 16/2);
+    windiesNum++;
+    windowOffset+=35/2;
+  }
 }
