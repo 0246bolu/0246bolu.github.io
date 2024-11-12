76acae7f0132fbf6ca65b45517b5ef20fcf4829a
Boyd4601148 <4601148@spsd.sk.ca>
2024-11-12 12:16:19 -0600

    Added BG Gif

--- Capstone Project/sketch.js
+++ Capstone Project/sketch.js
@@ -1,21 +1,29 @@
 let inMenu = true;
 let gameId = 0;
 let bgImage;
+let xScale 
+let yScale
 
 function preload(){
-  bgImage =  loadImage("Assets/MenuBackground.png");
+  bgImage = loadImage("/Assets/MenuGif.gif");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  imageMode(CENTER);
+  xScale = windowWidth/1365;
+  yScale = windowHeight/945;
+
 }
 
 function draw() {
   background(220);
   menu();
+  //console.log(mouseX, mouseY)
 }
 
 function menu(){
-  image(bgImage,windowWidth/2,windowHeight/2,  windowWidth, windowHeight); 
+  image(bgImage, 0, 0, windowWidth, windowHeight);
+  fill(0);
+  // rect(windowWidth/2-25, windowHeight/2-120, windowWidth/2, windowHeight/3+250);
+  //quad(324 * xScale, 52 * yScale, 994 * xScale, 52 * yScale , 984 * xScale, 732 * yScale,  334 * xScale, 732 * yScale);
 }
