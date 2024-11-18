3cea0988e2cb3990cfc44d2c43ac4138a4bd03d6
Boyd4601148 <4601148@spsd.sk.ca>
2024-11-18 12:22:56 -0600

    Finished project and added comments

--- Fractal Tree Project/sketch.js
+++ Fractal Tree Project/sketch.js
@@ -1,25 +1,32 @@
-let scale = 15;
+// The Balloon Tree
+// Lucas Boyd
+// 11/18/2024
+// Created a semi-fractal tree with random leaf colors that can be adjusted at will
+
+
+let scale = 15; // Set global variables to be used later
 let x = 5;
 let interval;
+let leaf;
+let randomChooser;
 
-function setup() {
+function setup() { // Creates canvas and chooses a random number to be used later
     createCanvas(500, 500);
-    background(255);
     interval = random(0,100);
 }
 
-function draw() {
+function draw() { // Runs every frame, sets what leaf its drawing to zero, and draws a tree with specific parameters
+    leaf = 0;
     background(255);
     drawTree(width/2, height*.9, 90, 6);
 }
 
-function drawLine(x1, y1, x2, y2, depth) {
-//draw a line segment connecting (x1,y1) to (x2,y2)
+function drawLine(x1, y1, x2, y2, depth) { //draw a line segment connecting (x1,y1) to (x2,y2) with varying thickness based on depth
     strokeWeight(depth*1.25);
     line(x1, y1, x2, y2);
 }
 
-function drawTree(x1, y1, angle, depth) {
+function drawTree(x1, y1, angle, depth) { // Draws tree and calls drawLeaf function
     if (depth > 0) {
         let x2 = x1 + (cos(radians(angle))*depth*scale); //calculate endpoints ofcurrent branch
         let y2 = y1 - (sin(radians(angle))*depth*scale); //using trig ratios. Getshorter based on depth
@@ -29,12 +36,11 @@ function drawTree(x1, y1, angle, depth) {
         drawTree(x2, y2, angle+spread, depth-1);
         drawTree(x2, y2, angle, depth-1);
         drawLeaf(x1,y1,x2,y2,depth);
+        leaf++;
     }
-    else{y=0;}
-    y++;
 }
 
-function keyPressed(){
+function keyPressed(){ // Runs when key pressed, if key is z, x is decreased and therefore the height required for a leaf is increased, opposite happens with x
     if(key==='z'){
         x--;
     }
@@ -43,8 +49,8 @@ function keyPressed(){
     }
 }
 
-function drawLeaf(x1,y1,x2,y2,depth){
-    randomSeed(y*interval*depth*x2);
+function drawLeaf(x1,y1,x2,y2,depth){ // Draws a leaf at specific size and color based on a random value and depth of its branch
+    randomSeed(leaf*interval);
     leafColor = [random(0,255),random(0,255),random(0,255)];
     fill(leafColor);
     let d = random(depth*5, depth*5+15)
