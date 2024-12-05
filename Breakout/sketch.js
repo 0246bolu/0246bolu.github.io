ec3dbf1affe0260f0b9324339dfc11ed7ab2645d
Boyd4601148 <4601148@spsd.sk.ca>
2024-12-05 11:26:48 -0600

    Implemented class for bricks

--- Breakout/sketch.js
+++ Breakout/sketch.js
@@ -1,4 +1,3 @@
-
 const NUM_ROWS = 8;
 const NUM_COLS = 14;
 let paddleX;
@@ -6,6 +5,8 @@ let paddleY;
 let pos;
 let vel;
 let bricks = [];
+let x = 0;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,28 +15,36 @@ function setup() {
   vel = createVector(5,3);
   paddleX = width/2;
   paddleY = height/2 + height/3;
+
+  for(let i = 0; i<NUM_ROWS; i++){
+    for(let j = 0; j<NUM_COLS; j++){
+      if(i===0||i===1){
+        fill(255,0,0);
+        bricks.push(new Brick(j*width/NUM_COLS+width/28, i*height/38+height/10, "red", x));
+      }
+      else if(i===2||i===3){
+        fill(255,127,0);
+        bricks.push(new Brick(j*width/NUM_COLS+width/28, i*height/38+height/10, "orange", x));
+      }
+      else if(i===4||i===5){
+        fill(0,255,0);
+        bricks.push(new Brick(j*width/NUM_COLS+width/28, i*height/38+height/10, "green", x));
+      }
+      else if(i===6||i===7){
+        fill(255,255,0);
+        bricks.push(new Brick(j*width/NUM_COLS+width/28, i*height/38+height/10, "yellow", x));
+      }
+      x++;
+    }
+  } 
 }
 
 function draw() {
   background(0);
   strokeWeight(width/95)
-  for(let i = 0; i<NUM_ROWS; i++){
-    for(let j = 0; j<NUM_COLS; j++){
-      if(i===0||i===1){
-        fill(255,0,0);
-      }
-      else if(i===2||i===3){
-        fill(255,127,0);
-      }
-      else if(i===4||i===5){
-        fill(0,255,0);
-      }
-      else if(i===6||i===7){
-        fill(255,255,0);
-      }
-      rect(j*width/NUM_COLS+width/28, i*height/38+height/10, width/NUM_COLS, height/40);
-    }
-  } 
+  for(i=0; i<bricks.length; i++){
+    bricks[i].display();
+  }
   fill(78,193,245);
   rect(paddleX, paddleY, width/NUM_COLS, height/40);
   if(keyIsDown(RIGHT_ARROW)){
@@ -52,10 +61,42 @@ function draw() {
 }
 
 class Brick{
-  constructor(posXBrick, posYBrick, color){
+  constructor(posXBrick, posYBrick, color, x){
     this.posXBrick = posXBrick;
     this.posYBrick = posYBrick;
     this.color = color;
+    this.x = x;
+  }
+  display(){
+    if(this.color === "red"){
+      fill(255,0,0);
+      rect(this.posXBrick, this.posYBrick, width/NUM_COLS, height/40);
+    }
+    else if(this.color === "orange"){
+      fill(255,127,0);
+      rect(this.posXBrick, this.posYBrick, width/NUM_COLS, height/40);
+    }
+    else if(this.color === "green"){
+      fill(0,255,0);
+      rect(this.posXBrick, this.posYBrick, width/NUM_COLS, height/40);
+    }
+    else if(this.color === "yellow"){
+      fill(255,255,0);
+      rect(this.posXBrick, this.posYBrick, width/NUM_COLS, height/40);
+    }
+
+    let brickLeft = this.posXBrick-width/NUM_COLS;
+    let brickRight = this.posXBrick+width/NUM_COLS;
+    let brickTop = this.posYBrick-height/40;
+    let brickBottom = this.posYBrick+height/40;
+    let left = pos.x-width/120;
+    let right = pos.x+width/120;
+    let top = pos.y-width/120;
+    let bottom = pos.y+width/120;
+    if(right>brickLeft && left<brickRight && top<brickBottom && bottom>brickTop){
+      vel.y *= -1;
+      bricks.splice(x,x);
+    }
   }
 }
 
