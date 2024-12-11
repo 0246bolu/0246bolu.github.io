eec62b1634d346c9aa3b1379a30a6e227d38ca70
Boyd4601148 <4601148@spsd.sk.ca>
2024-12-11 12:18:07 -0600

    Improved collision, added timer and counter along with other features

--- Breakout/sketch.js
+++ Breakout/sketch.js
@@ -6,10 +6,16 @@ let pos;
 let vel;
 let bricks = [];
 let breakCount = 0;
+let timer = 0;
+let timerTen = 0
+let timeMin = 0;
+let paddleSpeed = 7;
+let gameOver = false;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
+  textAlign(CENTER);
   pos = createVector(random(0,width), height/2);
   let velSign = Math.round(random());
   if(velSign===0){
@@ -23,19 +29,15 @@ function setup(){
   for(let i = 0; i<NUM_ROWS; i++){
     for(let j = 0; j<NUM_COLS; j++){
       if(i===0||i===1){
-        fill(255,0,0);
         bricks.push(new Brick(j*width/NUM_COLS+width/28, i*height/38+height/10, "red"));
       }
       else if(i===2||i===3){
-        fill(255,127,0);
         bricks.push(new Brick(j*width/NUM_COLS+width/28, i*height/38+height/10, "orange"));
       }
       else if(i===4||i===5){
-        fill(0,255,0);
         bricks.push(new Brick(j*width/NUM_COLS+width/28, i*height/38+height/10, "green"));
       }
       else if(i===6||i===7){
-        fill(255,255,0);
         bricks.push(new Brick(j*width/NUM_COLS+width/28, i*height/38+height/10, "yellow"));
       }
     }
@@ -52,14 +54,47 @@ function draw(){
   rect(paddleX, paddleY, width/NUM_COLS, height/40);
   if(keyIsDown(RIGHT_ARROW)){
     if(paddleX<width){
-      paddleX+=7;
+      paddleX+=paddleSpeed;
     }
   }
   else if(keyIsDown(LEFT_ARROW)){
     if(paddleX>0){
-      paddleX-=7;
+      paddleX-=paddleSpeed;
     }
   }
+  if(frameCount%60===0&&breakCount<112&&gameOver===false){
+    timer++;
+    if(timer>9){
+      timerTen++;
+      timer = 0;
+    }
+    if(timerTen>5){
+      timeMin++;
+      timerTen = 0;
+    }
+    paddleSpeed+=0.05;
+    if(vel.x>0){
+      vel.x+=0.05;
+      if(vel.y>0){
+        vel.y+=0.05;
+      }
+      else{
+        vel.y-=0.05;
+      }
+    }
+    else{
+      vel.x-=0.05;
+      if(vel.y>0){
+        vel.y+=0.05;
+      }
+      else{
+        vel.y-=0.05;
+      }
+    }
+  }
+  fill(255);
+  textSize(height/12);
+  text(timeMin+":"+timerTen+timer, width/2, height/13);
   ball();
 }
 
@@ -96,7 +131,6 @@ class Brick{
     let bottom = pos.y+width/120;
     if(right>brickLeft && left<brickRight && top<brickBottom && bottom>brickTop){
       if(vel.y>0&&(this.posYBrick-pos.y<=height/40*0.75||pos.y-this.posYBrick>=height/40/2)){
-        if(vel.y>0){
           if(vel.x<0){
             pos.x = brickRight+width/60/2;
           }
@@ -104,16 +138,15 @@ class Brick{
             pos.x = brickLeft-width/60/2;
           }
           vel.x *= -1;
+      }
+      else if(vel.y<0&&(this.posYBrick-pos.y>=height/40*0.75||pos.y-this.posYBrick<=height/40/2)){
+        if(vel.x<0){
+          pos.x = brickRight+width/60/2;
         }
         else{
-          if(vel.x<0){
-            pos.x = brickRight+width/60/2;
-          }
-          else{
-            pos.x = brickLeft-width/60/2;
-          }
-        vel.x *= -1;
+          pos.x = brickLeft-width/60/2;
         }
+        vel.x *= -1;
       }
       else{
         if(vel.y>0){
@@ -154,11 +187,18 @@ function ball(){
   if(pos.y<0){
     vel.y *= -1;
   }
-  if(pos.y>=height){
+  if(pos.y>=height&&breakCount<112){
     fill(255,0,0);
-    textAlign(CENTER);
     textSize(height/10);
     text("GAME OVER", width/2, height/2);
+    gameOver = true;
+  }
+  if(breakCount===112){
+    fill(0,255,0);
+    textSize(height/10);
+    text("YOU WIN", width/2, height/2);
   }
   rect(pos.x, pos.y, width/60);
+  textSize(height/12);
+  text(breakCount, width/13, height/13);
 }
