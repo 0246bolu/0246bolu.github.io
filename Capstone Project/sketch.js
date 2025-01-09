7f60d39e4216e5393f295a26f49b427d9f2bc08f
0246bolu <246bolu@gmail.com>
2025-01-09 11:00:28 -0600

    Started to attempt to link menu with game

--- Capstone Project/sketch.js
+++ Capstone Project/sketch.js
@@ -21,6 +21,10 @@ function draw() {
   //console.log(mouseX, mouseY)
 }
 
+function mouseClicked(){
+  location.replace("M:/Github Repo/0246bolu.github.io/0246bolu.github.io/Space Invaders/index.html")
+}
+
 function menu(){
   image(bgImage, 0, 0, windowWidth, windowHeight);
   fill(0);
